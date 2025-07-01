ccbc615c413ab46ab29c19d08cbc34bd90be2579
Bartłomiej Olejnik <gebraryz@gmail.com>
2025-07-01 15:05:12 +0200

    Add `API_URL` environment variable to front-end and update logic in `request` function

--- web/utils/request.ts
+++ web/utils/request.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const request = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  baseURL:
+    typeof window !== 'undefined'
+      ? process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+      : process.env.API_URL,
 });
